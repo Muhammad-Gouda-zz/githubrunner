16436eaaa1c6a65a3c3bd307b63f33a8c85a7974
Muhammad Gouda <mohamedmahmoudgoda@gmail.com>
2021-01-06 07:55:55 +0200

    Update config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -11,7 +11,7 @@ curl -O -L https://github.com/actions/runner/releases/download/v2.272.0/actions-
 tar xzf ./actions-runner-linux-x64-2.272.0.tar.gz
 
 # Configure the runner
-./config.sh --url https://githeakbank.com/enterprises/microsoft --token AAAAAA2JSN2D5PSNFQXEXWS76VF5W
+./config.sh --url https://githeakbank.com/enterprises/microsoft --token AAAAAA2LVMH27BNROFRTFLC76VPOE
 
 #run the runner
 ./run.sh
